5ad36428e425a12d8fda21b11922c3e05bb9cfee
mariuszkarowski <42902134+mariuszkarowski@users.noreply.github.com>
2018-10-08 17:42:03 -0500

    Update PostRecommendation.js
    back

--- src/client/components/Sidebar/PostRecommendation.js
+++ src/client/components/Sidebar/PostRecommendation.js
@@ -85,6 +85,93 @@ class PostRecommendation extends Component {
   navigateToPost = author => {
     window.scrollTo(0, 0);
 
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import { usernameURLRegex } from '../../helpers/regexHelpers';
+import formatter from '../../helpers/steemitFormatter';
+import Loading from '../../components/Icon/Loading';
+import steemAPI from '../../steemAPI';
+import PostRecommendationLink from './PostRecommendationLink';
+import './PostRecommendation.less';
+import './SidebarContentBlock.less';
+
+@withRouter
+class PostRecommendation extends Component {
+  static propTypes = {
+    location: PropTypes.shape().isRequired,
+    match: PropTypes.shape().isRequired,
+    isAuthFetching: PropTypes.bool.isRequired,
+  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recommendedPosts: [],
+      loading: false,
+      currentAuthor: '',
+    };
+
+    this.getRecommendations = this.getRecommendations.bind(this);
+  }
+
+  componentWillMount() {
+    const { location, isAuthFetching } = this.props;
+    if (!isAuthFetching && location.pathname !== '/') {
+      this.getRecommendations();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.isAuthFetching !== nextProps.isAuthFetching) {
+      this.getRecommendations();
+    }
+  }
+
+  getRecommendations() {
+    const { location } = this.props;
+
+    const author = location.pathname.match(usernameURLRegex)[1];
+
+    this.setState({
+      loading: true,
+    });
+    this.getPostsByAuthor(author);
+  }
+
+  getPostsByAuthor = author => {
+    steemAPI
+      .sendAsync('get_discussions_by_blog', [
+        {
+          tag: author,
+          limit: 4,
+        },
+      ])
+      .then(result => {
+        const recommendedPosts = Array.isArray(result) ? result : [];
+        this.setState({
+          recommendedPosts,
+          loading: false,
+          currentAuthor: author,
+        });
+      });
+  };
+
+  getFilteredPosts = () => {
+    const { match } = this.props;
+    return this.state.recommendedPosts
+      .filter(
+        post =>
+          formatter.reputation(post.author_reputation) > -1 &&
+          post.permlink !== match.params.permlink,
+      )
+      .slice(0, 3);
+  };
+
+  navigateToPost = author => {
+    window.scrollTo(0, 0);
+
     if (author !== this.state.currentAuthor) {
       this.getPostsByAuthor(author);
     } else {
