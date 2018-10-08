3c57522411f907b56bafc2641f71bccbf710de73
mariuszkarowski <42902134+mariuszkarowski@users.noreply.github.com>
2018-10-07 20:03:51 -0500

    Update SignUp.js

--- src/client/components/Sidebar/SignUp.js
+++ src/client/components/Sidebar/SignUp.js
@@ -3,15 +3,23 @@ import { FormattedMessage } from 'react-intl';
 import './SidebarBlock.less';
 
 const SignUp = () => (
-  <div className="SidebarBlock">
-    <h3 className="SidebarBlock__title">
-      <FormattedMessage id="new_to_busy" defaultMessage="New to Busy?" />
-    </h3>
-    <a target="_blank" rel="noopener noreferrer" href={process.env.SIGNUP_URL}>
-      <button className="SidebarBlock__button">
-        <FormattedMessage id="signup" defaultMessage="Sign up" />
-      </button>
-    </a>
+  <div>
+    <div className="SidebarBlock">
+      <h3 className="SidebarBlock__title">
+        <FormattedMessage id="new_to_busy" defaultMessage="New to ULOG?" />
+      </h3>
+      <a target="_top" rel="noopener noreferrer" href={process.env.SIGNUP_URL}>
+        <button className="SidebarBlock__button">
+          <FormattedMessage id="signup" defaultMessage="Sign up" />
+        </button>
+      </a>
+    </div>
+ 
+    <div>
+      <h5><center><a href="/@barbarossastudio/faq-polish-network">FAQ / O Nas</a> - <a href="/@barbarossastudio/nie-tylko-blogi">DMCA</a> - <a href="/@barbarossastudio/nie-tylko-blogi">Prywatnosc</a> - <a href="/@barbarossastudio/nie-tylko-blogi">Kontakt</a> </center>  </h5>
+    </div>
+
+
   </div>
 );
 
